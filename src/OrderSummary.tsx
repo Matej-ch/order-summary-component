8c75b6332288a6bf00034a26f58c51f57a2e6cf1
Matej <matejchalachan@gmail.com>
2022-12-18 23:00:06 +0100

    finished component

--- src/OrderSummary.tsx
+++ src/OrderSummary.tsx
@@ -5,22 +5,25 @@ function OrderSummary() {
     return (<div className='summary-container'>
         <div className={'background'}></div>
 
-        <h1>Order summary</h1>
-        <p>You can now listen to millions of songs audiobooks, and podcasts on any device anywhere you like!</p>
+        <div className={'info-container'}>
+            <h1>Order summary</h1>
+            <p>You can now listen to millions of songs audiobooks, and podcasts on any device anywhere you like!</p>
 
-        <div>
-            <img src={iconMusic} alt=""/>
-            <div><span>Annual Plan</span> <span>$59.99/year</span></div>
-            <div><a href="#">Change</a></div>
-        </div>
+            <div className={'plan'}>
+                <img src={iconMusic} alt=""/>
+                <div><span className={'plan-type'}>Annual Plan</span> <span className={'plan-price'}>$59.99/year</span>
+                </div>
+                <div><a href="#">Change</a></div>
+            </div>
 
-        <div>
-            <button className={'proceed'}>Proceed to Payment</button>
-        </div>
+            <div>
+                <button className={'proceed'}>Proceed to Payment</button>
+            </div>
+            
+            <div>
+                <button className={'cancel'}>Cancel Order</button>
+            </div>
 
-
-        <div>
-            <button className={'cancel'}>Cancel Order</button>
         </div>
 
 
